5965fe08abb97fa4385d40112eec5de52f8f6097
monks <andrew.monks@samsara.com>
2019-10-17 03:13:40 +0000

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+
+set fish_greeting "🐋 🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋"
+
+fish_vi_key_bindings
+
+set -x FZF_DEFAULT_COMMAND 'fd --type f -I'
+
+set -x PATH $PATH ~/bin
+
+eval (direnv hook fish)
+
+alias config='/usr/bin/git --git-dir=/home/ubuntu/.cfg/ --work-tree=/home/ubuntu'
+
