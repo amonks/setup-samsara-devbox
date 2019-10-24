65757239ab5dca3dfb4bf5dfc8b6b9e5db2208e5
monks <andrew.monks@samsara.com>
2019-10-24 23:32:28 +0000

    tabs->spaces, install neovim node host

--- setup.sh
+++ setup.sh
@@ -7,15 +7,15 @@ set -e
 trap "[[ \$BASH_COMMAND != echo* && \$BASH_COMMAND != section* ]] && echo \$(prompted \$BASH_COMMAND)" DEBUG
 
 function prompted() {
-	echo -e "\e[32m> $@\e[39m"
+  echo -e "\e[32m> $@\e[39m"
 }
 
 function section() {
-	echo -e "\n\n\n\e[1m\e[35m$@\e[39m\e[0m\n"
+  echo -e "\n\n\n\e[1m\e[35m$@\e[39m\e[0m\n"
 }
 
 function end_section() {
-	echo -e "\n\e[1m\e[35mDone.\e[39m\e[0m\n"
+  echo -e "\n\e[1m\e[35mDone.\e[39m\e[0m\n"
 }
 
 
@@ -36,57 +36,72 @@ echo
 
 
 if ! which fish; then
-	section Installing fish shell
-	apt-get install fish -y
-	end_section
+  section Installing fish shell
+  apt-get install fish -y
+  end_section
 fi
 echo
 
 if ! grep -q fish /etc/shells; then
-	section Approving fish shell
-	echo "/usr/bin/fish" | tee -a /etc/shells
-	end_section
+  section Approving fish shell
+  echo "/usr/bin/fish" | tee -a /etc/shells
+  end_section
 fi
 echo
 
 if ! which nvim; then
-	section Installing neovim
-	apt-get install neovim  -y
-	end_section
+  section Installing neovim
+  apt-get install neovim  -y
+  end_section
 fi
 echo
 
 if ! which rg; then
-	section Installing ripgrep
-	rg_version=11.0.2
-	wget https://github.com/BurntSushi/ripgrep/releases/download/${rg_version}/ripgrep_${rg_version}_amd64.deb
-	apt install ./ripgrep_${rg_version}_amd64.deb
-	rm ./ripgrep_${rg_version}_amd64.deb
-	end_section
+  section Installing ripgrep
+  rg_version=11.0.2
+  wget https://github.com/BurntSushi/ripgrep/releases/download/${rg_version}/ripgrep_${rg_version}_amd64.deb
+  apt install ./ripgrep_${rg_version}_amd64.deb
+  rm ./ripgrep_${rg_version}_amd64.deb
+  end_section
 fi
 echo
 
 if ! which fd; then
-	section Installing fd
-	fd_version=7.4.0
-	wget https://github.com/sharkdp/fd/releases/download/v${fd_version}/fd_${fd_version}_amd64.deb
-	apt install ./fd_${fd_version}_amd64.deb
-	rm ./fd_${fd_version}_amd64.deb
-	end_section
+  section Installing fd
+  fd_version=7.4.0
+  wget https://github.com/sharkdp/fd/releases/download/v${fd_version}/fd_${fd_version}_amd64.deb
+  apt install ./fd_${fd_version}_amd64.deb
+  rm ./fd_${fd_version}_amd64.deb
+  end_section
 fi
 echo
 
 if ! test -f /usr/share/autojump/autojump.fish; then
-	section Installing autojump
-	apt-get install autojump -y
-	end_section
+  section Installing autojump
+  apt-get install autojump -y
+  end_section
 fi
 echo
 
 if ! which mosh; then
-	section Installing mosh
-	apt-get install mosh -y
-	end_section
+  section Installing mosh
+  apt-get install mosh -y
+  end_section
+fi
+echo
+
+if ! which node; then
+  section Installing node
+  curl -sL https://deb.nodesource.com/setup_13.x | bash -
+  apt-get install -y nodejs
+  end_section
+fi
+echo
+
+if ! which neovim-node-host; then
+  section Installing neovim node host
+  npm i -g neovim
+  end_section
 fi
 echo
 
