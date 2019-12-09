60e3993a09800cbd63b8fbfe7e99c39e39e04966
monks <andrew.monks@samsara.com>
2019-12-09 22:04:59 +0000

    rewrite cherry-pick script

--- .config/fish/functions/cherry-pick.fish
+++ .config/fish/functions/cherry-pick.fish
@@ -1,18 +1,49 @@
 function cherry-pick
-    set branch "$argv"
-    echo "cherry picking into $branch"
+    set branch "$argv[1]"
+    set label "$argv[2]"
 
+    if test -z "$branch"
+      echo "must provide branch"
+      echo "[set ~/cherry-picks to git log]; cherry-pick u50 fix-thingy"
+      return 1
+    end
+
+    if test -z "$label"
+      echo "must provide label"
+      return 1
+    end
+
+    echo
+    echo "cherry picking into $branch, and calling it $label"
+
+    echo
+    echo "checking out $branch"
     git summon "$branch"
-    git checkout "$branch"
+    git checkout -f "$branch"
+
+    echo
+    echo "resetting $branch"
     git reset --hard "origin/$branch"
 
-    git checkout -b "amonks/cp-$branch"
+    echo
+    set new_branch "amonks/cp-$label-$branch"
+    echo "making new branch $new_branch"
+    git checkout -b "$new_branch"
 
+    echo
+    echo "looping through commits"
     set picks (cat ~/cherry-picks)
     for pick in $picks
-        set sha (echo $pick | cut -d' ' -f1)
-        git cherry-pick $sha
+      echo
+      echo "handling commit $pick"
+      set sha (echo $pick | cut -d' ' -f1)
+      echo
+      echo "cherry picking sha $sha"
+      git cherry-pick $sha
     end
 
     git p
+    echo "URL TO MAKE CHERRY PICK PR INTO CORRECT BRANCH:"
+    echo "https://github.com/samsara-dev/backend/compare/$branch...$new_branch?expand=1"
 end
+
